436608d8f15c62c6b0f6ef081d752de8c577d696
Remy Phan <phan.remy@gmail.com>
2019-11-12 16:43:30 +0100

    coolseb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  article = Article.new(title: Faker::Games::Pokemon.name, content: Faker::Quote.matz)
+  article.save
+end
